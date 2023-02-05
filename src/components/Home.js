786fe652f7504cb9ae2ed47b3fff1d24d68f33bd
oscar 123 <oscar7486378@gmail.com>
2023-02-04 19:49:46 -0500

    created containers for different pages

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <p>
+                Welcome to Oscar's website
+            </p>
+        </div>
+    )
+}
+
+export default Home;
